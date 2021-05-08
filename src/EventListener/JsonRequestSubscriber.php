aa58a74fd5d298da4528f6ef5ef0556b6d296438
Lars Graubner <mail@larsgraubner.de>
2021-05-08 21:27:55 +0200

    rename

--- src/EventListener/JsonRequestSubscriber.php
+++ src/EventListener/JsonRequestSubscriber.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\EventSubscriber;
+namespace App\EventListener;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
